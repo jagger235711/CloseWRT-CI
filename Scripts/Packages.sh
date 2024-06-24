801a1adf82e09ed35ad36ef2965dee5cff35f862
巫文杰 <252587809@qq.com>
2024-06-24 11:53:47 +0800

    配置调整：更新Packages.sh以切换Passwall版本

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -26,7 +26,8 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "master"
 
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
-UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
+# UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
+UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
