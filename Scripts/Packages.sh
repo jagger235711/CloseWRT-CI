2729e79fa72913fbb72185f310c170802c6711ca
巫文杰 <252587809@qq.com>
2024-07-23 12:26:34 +0800

    更新包管理配置，调整插件包，优化项目结构。

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -26,7 +26,7 @@ UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "js"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-# UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
